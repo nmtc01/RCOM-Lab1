8c4a8bab217f76827a2a343beda376f202dffbb6
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2019-10-14 16:37:05 +0100

    Disc message done without time_out

--- src/receiver.h
+++ src/receiver.h
@@ -14,10 +14,11 @@
 
 // FLAGS
 #define FLAG 0x7E
-#define A_CMD 0x03
-#define A_ANS 0x01
+#define A_ANS 0x03
+#define A_CMD 0x01
 #define C_UA 0x07
 #define C_SET 0x03
+#define C_DISC 0x0B
 
 // SIZES
 #define STR_SIZE 255
@@ -37,6 +38,9 @@ void message(char* message);
 void setup(int argc, char **argv);
 void open_port(char **argv, int *fd_ptr);
 void set_flags(struct termios *oldtio_ptr, struct termios *newtio_ptr, int *fd_ptr);
-int read_msg(int *fd_ptr, unsigned char *buf);
-int write_msg(int *fd_ptr, int n_bytes);
+int read_set(int *fd_ptr, unsigned char *buf);
+int write_ua(int *fd_ptr, int n_bytes);
+int read_disc(int *fd_ptr, unsigned char *request);
+int write_disc(int *fd_ptr, int n_bytes);
+int read_ua(int *fd_ptr, unsigned char *answer);
 void cleanup(struct termios *oldtio_ptr, int *fd_ptr);
